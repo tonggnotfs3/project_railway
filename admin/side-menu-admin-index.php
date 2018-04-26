c727bd383cd1ca54fff372c1690c7d864a197186
TONGz <tongfs33@gmail.com>
2018-04-26 19:35:28 +0700

    maybe finish

--- admin/side-menu-admin-index.php
+++ admin/side-menu-admin-index.php
@@ -68,7 +68,7 @@ function loadDoc4() {
 </script> -->
 <div class="list-group">
   <a href="#" class="list-group-item active list-group-item-warning">ประเภท</a>
-  <a data-toggle="collapse" href="#collapse1"  class="list-group-item"><span class="glyphicon glyphicon-bullhorn"></span>&nbsp;ข้อมูลที่ที่ยังไม่ได้จัดประเภท</a>
+  <a data-toggle="collapse" href="#collapse1"  class="list-group-item">&nbsp;ข้อมูลที่ที่ยังไม่ได้จัดประเภท</a>
     <div id="collapse1" class="panel-collapse collapse">
       <a href="display_complain_admin.php"  class="list-group-item"><span class="glyphicon glyphicon-bullhorn"></span>&nbsp;ร้องเรียน-ร้องทุกข์</a>
       <a href="display_sug_admin.php"  class="list-group-item"><span class="glyphicon glyphicon-pencil"></span>&nbsp;แนะนำ</a>
@@ -77,23 +77,23 @@ function loadDoc4() {
     </div>
 
 
-  <a data-toggle="collapse" href="#collapse2"  class="list-group-item"><span class="glyphicon glyphicon-pencil"></span>&nbsp;ข้อมูลแยกตามหน่วยงาน</a>
+  <a data-toggle="collapse" href="#collapse2"  class="list-group-item">&nbsp;ข้อมูลแยกตามหน่วยงาน</a>
   <div id="collapse2" class="panel-collapse collapse">
-    <a data-toggle="collapse" href="#collapse21"  class="list-group-item"><span class="glyphicon glyphicon-bullhorn"></span>&nbsp;หน่วยงานที่ 1</a>
+    <a data-toggle="collapse" href="#collapse21"  class="list-group-item">&nbsp;หน่วยงานที่ 1</a>
       <div id="collapse21" class="panel-collapse collapse">
         <a href="unit_display_complain_admin.php?responsible=1"  class="list-group-item"><span class="glyphicon glyphicon-bullhorn"></span>&nbsp;ร้องเรียน-ร้องทุกข์</a>
         <a href="unit_display_sug_admin.php?responsible=1"  class="list-group-item"><span class="glyphicon glyphicon-pencil"></span>&nbsp;แนะนำ</a>
         <a href="unit_display_danger_admin.php?responsible=1"  class="list-group-item"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp;แจ้งแหตุอันตราย</a>
         <a href="unit_display_admire_admin.php?responsible=1"  class="list-group-item"><span class="glyphicon glyphicon-thumbs-up"></span>&nbsp;ชื่นชม</a>
       </div>
-    <a data-toggle="collapse" href="#collapse22"  class="list-group-item"><span class="glyphicon glyphicon-pencil"></span>&nbsp;หน่วยงานที่ 2</a>
+    <a data-toggle="collapse" href="#collapse22"  class="list-group-item"></span>&nbsp;หน่วยงานที่ 2</a>
       <div id="collapse22" class="panel-collapse collapse">
         <a href="unit_display_complain_admin.php?responsible=2"  class="list-group-item"><span class="glyphicon glyphicon-bullhorn"></span>&nbsp;ร้องเรียน-ร้องทุกข์</a>
         <a href="unit_display_sug_admin.php?responsible=2"  class="list-group-item"><span class="glyphicon glyphicon-pencil"></span>&nbsp;แนะนำ</a>
         <a href="unit_display_danger_admin.php?responsible=2"  class="list-group-item"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp;แจ้งแหตุอันตราย</a>
         <a href="unit_display_admire_admin.php?responsible=2"  class="list-group-item"><span class="glyphicon glyphicon-thumbs-up"></span>&nbsp;ชื่นชม</a>
       </div>
-    <a data-toggle="collapse" href="#collapse23"  class="list-group-item"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp;หน่วยงานที่ 3</a>
+    <a data-toggle="collapse" href="#collapse23"  class="list-group-item">&nbsp;หน่วยงานที่ 3</a>
       <div id="collapse23" class="panel-collapse collapse">
         <a href="unit_display_complain_admin.php?responsible=3"  class="list-group-item"><span class="glyphicon glyphicon-bullhorn"></span>&nbsp;ร้องเรียน-ร้องทุกข์</a>
         <a href="unit_display_sug_admin.php?responsible=3"  class="list-group-item"><span class="glyphicon glyphicon-pencil"></span>&nbsp;แนะนำ</a>
@@ -103,11 +103,11 @@ function loadDoc4() {
   </div>
 
 
-  <a data-toggle="collapse" href="#collapse3"  class="list-group-item"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp;ข้อมูลที่ได้รับการจัดการแล้ว</a>
+  <a data-toggle="collapse" href="#collapse3"  class="list-group-item">&nbsp;ข้อมูลที่ได้รับการจัดการแล้ว</a>
   <div id="collapse3" class="panel-collapse collapse">
     <a href="display_complain_admin.php"  class="list-group-item"><span class="glyphicon glyphicon-bullhorn"></span>&nbsp;ร้องเรียน-ร้องทุกข์</a>
     <a href="display_sug_admin.php"  class="list-group-item"><span class="glyphicon glyphicon-pencil"></span>&nbsp;แนะนำ</a>
     <a href="display_danger_admin.php"  class="list-group-item"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp;แจ้งแหตุอันตราย</a>
-    <a href="display_admire_admin.php"  class="list-group-item"><span class="glyphicon glyphicon-thumbs-up"></span>&nbsp;ชื่นชม</a>
+    <a href="fin_display_admire_admin.php"  class="list-group-item"><span class="glyphicon glyphicon-thumbs-up"></span>&nbsp;ชื่นชม</a>
   </div>
 </div>
