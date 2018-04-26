460815b10f8fd80185a0fcb0cb528970b12f1fa6
TONGz <tongfs33@gmail.com>
2018-04-26 20:25:10 +0700

    change value

--- admin/selectStatus.php
+++ admin/selectStatus.php
@@ -1,9 +1,9 @@
 <select id="status<?=$i;?>" name="status<?=$i;?>">
-  <option value="1" <?php if($row['status'] == 1){
+  <option value="0" <?php if($row['status'] == 0){
     echo "selected";
   } ?>>ยังไม่ได้ดำเนินการ
   </option>
-  <option value="2" <?php if($row['status'] == 2){
+  <option value="1" <?php if($row['status'] == 1){
     echo "selected";
   } ?>>ดำเดินการเสร็จสิ้นแล้ว
   </option>
